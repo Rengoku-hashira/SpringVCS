ce9ca9549bb7306ba44a132653786e8d7ee6d255
Rengoku-hashira <166949131+Rengoku-hashira@users.noreply.github.com>
2024-06-06 21:22:09 +0300

    Add files via upload

---
+++ VCS(spargalke).js
@@ -0,0 +1,38 @@
+// FIBONACHI
+
+// let pirmasSkaicius = 0;  
+// let antrasSkaicius = 1;  
+// let fibSuma = pirmasSkaicius + antrasSkaicius; 
+// //console.log(pirmasSkaicius);
+// console.log(antrasSkaicius);
+// console.log(fibSuma);
+// let n = 10;
+// for (let i = 2; i < n; i++) {
+//     pirmasSkaicius = antrasSkaicius;
+//     antrasSkaicius = fibSuma;
+//     fibSuma = pirmasSkaicius + antrasSkaicius;
+//     console.log(fibSuma);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// && (description) - and(ir) + example (x< 10 && y > 1) is true
+// || (description) - or(arba) + example (x == 5 || y == 5) is false
+// ! (description) - not(ne) + example !(x == y) is true
